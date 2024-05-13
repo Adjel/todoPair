d823ae222e98c026471791eb8971ca67f5613cd7
Adjel <adjel.touaoula@gmail.com>
2024-05-13 14:38:38 +0200

    register front

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 export default function Register() {
+  const [credential, setCredential] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleOnchange(event) {
+    const { name, value } = event.target;
+    setCredential((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
+
   return (
     <div>
+      <form>
+        <label htmlFor="email">email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={credential.email}
+          onChange={(event) => handleOnchange(event)}
+        ></input>
+        <label htmlFor="password">password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={credential.password}
+          onChange={(event) => handleOnchange(event)}
+        ></input>
+      </form>
       <Link href={"/Login"}>login</Link>
     </div>
   );
