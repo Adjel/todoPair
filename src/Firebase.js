ea1f6b9ce0a4dbc2984ad47e2b4aa63ba4105129
Adjel <adjel.touaoula@gmail.com>
2024-05-13 14:29:26 +0200

    firebase and env

---
+++ src/Firebase.js
@@ -0,0 +1,60 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  onSnapshot,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// without it the API KEY in the POST will be import.meta.envVITE_API_KEY and response will be 404
+const env = await import.meta.env;
+
+const firebaseConfig = {
+  apiKey: env.VITE_API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: "todoreact-6443e",
+  //projectId: "import.meta.env.PROJECT_ID",
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export {
+  collection,
+  addDoc,
+  serverTimestamp,
+  onSnapshot,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  doc,
+  deleteDoc,
+  updateDoc,
+};
+
+export default app;
